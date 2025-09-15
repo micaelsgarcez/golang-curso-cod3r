f40d78ae3edf565f465dce6deb8d811c042215c7
Micael Garcez <micaelsgarcez@gmail.com>
2025-09-15 08:26:43 -0300

    Controles switch 2 e desafio de refatorar o primeiro switch

--- controles/switch/switch.go
+++ controles/switch/switch.go
@@ -1,21 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func notaParaConceito(n float64) string {
 	var nota = int(n)
-	switch nota {
-		case 10:
-			fallthrough
-		case 9:
+	switch {
+		case nota >= 9 && nota < 11:
 			return "A"
-		case 8, 7:
+		case nota >= 7 && nota < 9:
 			return "B"
-		case 6, 5:
+		case nota >= 5 && nota < 7:
 			return "C"
-		case 4, 3:
+		case nota >= 3 && nota < 5:
 			return "D"
-		case 2, 1, 0:
+		case nota >= 0 && nota < 3:
 			return "E"
 		default:
 			return "Nota inválida"
@@ -23,7 +24,17 @@ func notaParaConceito(n float64) string {
 }
 
 func main() {
-	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(11.8))
 	fmt.Println(notaParaConceito(6.8))
 	fmt.Println(notaParaConceito(2.8))
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Bom dia!")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde!")
+	default:
+		fmt.Println("Boa noite!")
+	}
 }
