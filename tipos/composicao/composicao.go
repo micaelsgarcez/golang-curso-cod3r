d18f58b45b61e92aee3865a1d5377b01313542f5
Micael Garcez <micaelsgarcez@gmail.com>
2025-09-17 08:35:59 -0300

    Tipos composição de interfaces

---
+++ tipos/composicao/composicao.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+	// pode adicionar mais metodos
+}
+
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("baliza...")
+}
+
+func main() {
+	var b esportivoLuxuoso = bmw7{}
+	b.ligarTurbo()
+	b.fazerBaliza()
+}
